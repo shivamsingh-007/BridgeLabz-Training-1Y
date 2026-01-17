fe7c3980e890fd289d331cb1bff3d63d2a81ef32
shivam singh <shivam.singh.koyad2006@gmail.com>
2026-01-17 22:06:07 +0530

    Add files via upload

---
+++ agefinder.java
@@ -0,0 +1,8 @@
+public class agefinder{
+    public static void main(String[] args){
+        int YOB = 2000;
+        int currentYear = 2024;
+        int age = currentYear - YOB;
+        System.out.println("Age: " + age);
+    }
+}
