502913e5fd6653eb2ab3b51213d6173d26c34d05
shivam singh <shivam.singh.koyad2006@gmail.com>
2026-01-18 20:18:45 +0530

    km to miles

--- km_to_mile.java
+++ km_to_mile.java
@@ -2,6 +2,6 @@ public class km_to_mile {
     public static void main(String[] args) {
         double km = 10.8;
         double mile = km * 1.6;
-        System.out.println("Distance in miles: " + mile);
+        System.out.printf("Distance %f in km is %f in  miles: " , km, mile);
     }
 }
