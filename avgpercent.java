4f7b31a7f9c2f35af0e591dc9f7899e740e528da
shivam singh <shivam.singh.koyad2006@gmail.com>
2026-01-18 11:35:46 +0530

    avgpercent

---
+++ avgpercent.java
@@ -0,0 +1,12 @@
+public class avgpercent {
+    public static void main(String[] args) {
+        int phy = 95;
+        int chem = 96;
+        int math = 94;
+        int total = phy + chem + math;
+        double avgpercent = total / 3.0;
+
+        System.out.println("Average percentage: " + avgpercent);
+    }
+    
+}
