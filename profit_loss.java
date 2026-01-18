c1467db1bd0e487415189058d81dace5310fefb2
shivam singh <shivam.singh.koyad2006@gmail.com>
2026-01-18 20:23:53 +0530

    profit loss percentage

--- profit_loss.java
+++ profit_loss.java
@@ -2,15 +2,8 @@ public class profit_loss {
     public static void main(String[] args){
         int cp = 129;
         int sp = 191;
-        if(sp > cp){
-            int profit_percent = ((sp - cp) * 100) / cp;
-            System.out.println("Profit Percentage: " + profit_percent + "%");
-        } else if(cp > sp){
-            int loss_percent = ((cp - sp) * 100) / cp;
-            System.out.println("Loss Percentage: " + loss_percent + "%");
-        } else {
-            System.out.println("No Profit No Loss");
-        }
+        System.out.printf("The Cost Price is INR %d and Selling Price is INR %d\n", cp, sp);
+        System.out.printf("The Profit is INR %d and the Profit Percentage is %d", (sp - cp), ((sp - cp) * 100) / cp);
     }
     
 }
