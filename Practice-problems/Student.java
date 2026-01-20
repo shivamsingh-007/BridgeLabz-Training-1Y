785831533c1c3c236bfe733d2ba0a8e78019dc8c
shivam singh <shivam.singh.koyad2006@gmail.com>
2026-01-20 09:31:33 +0530

    Fix: remove embedded repo and track files normally

---
+++ Practice-problems/Student.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Student {
+static void student_info(String name, int age,int id_roll){
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("ID Roll: " + id_roll);
+}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter student id roll:");
+        int id_roll = input.nextInt();
+        System.out.println("Enter student age:");
+        int age = input.nextInt();
+        System.out.println("Enter student name:");
+        String name = input.next();
+
+
+
+        student_info(name, age, id_roll);
+    }
+}
