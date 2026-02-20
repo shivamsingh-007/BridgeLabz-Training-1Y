148b87fc6ad9f37a4a97a95956107a3f2e82a8c2
shivam singh <shivam.singh.koyad2006@gmail.com>
2026-02-20 22:53:06 +0530

    Delete agefinder.java

--- agefinder.java
+++
@@ -1,8 +0,0 @@
-public class agefinder{
-    public static void main(String[] args){
-        int YOB = 2000;
-        int currentYear = 2024;
-        int age = currentYear - YOB;
-        System.out.println("Age: " + age);
-    }
-}
